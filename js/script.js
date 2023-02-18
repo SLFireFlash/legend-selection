0c7de4c80058b4f11e16204b5b26638cc8644396
SLFireFlash <sllahiruprasanna@gmail.com>
2023-02-18 11:29:34 +0530

    class one completed

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+function allLegneds(){
+    menuBody =document.getElementById("menu-body")
+    // divlrow01 = document.createElement("div")
+    // divlrow01.setAttribute("class","menu-body container")
+
+    // divclass01 = document.createElement("div")
+    // divclass01.setAttribute("class","legend-row-01 d-flex container")
+
+    // divlrow01.appendChild(divclass01);
+    // menuBody.appendChild(divlrow01);
+    var meneEle ="<div class=\"legend-row-01 d-flex container justify-content-center\"><div class=\"class-01-body d-flex\"><div class=\"box\">legend</div><div class=\"box\">legend</div><div class=\"box\">legend</div><div class=\"box\">legend</div><div class=\"box\">legend</d></div><div class=\"class-02-body d-flex\"><div class=\"box\">legend 1</div><div class=\"box\">legend 2</div><div class=\"box\">legend 3</div><div class=\"box\">legend 4</div><div class=\"box\">legend 5</div><div class=\"box\">legend 6</div></div></div></div><div class=\"legend-row-02 d-flex \"><div class=\"class-03-body d-flex\"><div class=\"box\">legend 1</div><div class=\"box\">legend 2</div><div class=\"box\">legend 3</div><div class=\"box\">legend 4</div></div><div class=\"class-04-body d-flex\"><div class=\"box\">legend 1</div><div class=\"box\">legend 2</div><div class=\"box\">legend 3</div><div class=\"box\">legend 4</div></div><div class=\"class-05-body d-flex\"><div class=\"box\">legend 1</div><div class=\"box\">legend 2</div><div class=\"box\">legend 3</div><div class=\"box\">legend 4</div></div></div></div> ";
+    menuBody.innerHTML = meneEle;
+}
+
+function "LegnedClass01(){
+    
+}
